e7b9b095496d928488f8d7f1d99b428d6758ff28
ElianoDeLaFlor <48012583+ElianoDeLaFlor@users.noreply.github.com>
2020-01-21 15:50:25 +0000

    Create Unite.cs

---
+++ ChiffreEnLettre/Unite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChiffreEnLettre
+{
+    class Unite
+    {
+        public static long Unit { get; set; }
+
+        /// <summary>
+        /// Contient le nom en lettre des chiffres des unités
+        /// </summary>
+        private static readonly string[] TableauUnite = new[] { "Zéro", "Un", "Deux", "Trois", "Quatre", "Cinq", "Six", "Sept", "Huit", "Neuf" };
+
+        public static string InLetter(long unit)
+        {
+            return TableauUnite[unit];
+        }
+    }
+}
