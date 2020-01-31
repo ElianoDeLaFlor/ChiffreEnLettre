ca58dd150ba9b6b4c9fb1aa69976bfe68e64aa65
ElianoDeLaFlor <48012583+ElianoDeLaFlor@users.noreply.github.com>
2020-01-31 15:58:47 +0000

    Create EnLettre.cs

---
+++ ChiffreEnLettre/EnLettre.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChiffreEnLettre
+{
+    public static class EnLettre
+    {
+        public static string Chiffre { get; set; }
+
+        public static string  Resultat{ get { return Result(); } }
+
+        private static string Result()
+        {
+            //juste pour faire les testes
+            long k = Convert.ToInt64(Chiffre);
+            string rslt;
+            if(k.IsUnite())
+            {
+                //unite
+                
+                rslt=Unite.InLetter(k);
+            }
+            else if(k.IsDizaine())
+            {
+                //Dizaine
+                rslt = Dizaine.DizainesInLetter(k);
+            }
+            else if(k.IsCentaine())
+            {
+                //centaine
+                rslt = Centaine.CentaineInLetter(k);
+            }
+            else
+            {
+                //Millieme
+                rslt = Millieme.MilleInLetter(k);
+            }
+            return rslt;
+        }
+        
+    }
+}
