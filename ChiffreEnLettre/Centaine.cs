c3ce2907131670b0b3949989e77bdfa1e3444959
ElianoDeLaFlor <48012583+ElianoDeLaFlor@users.noreply.github.com>
2020-01-31 15:58:28 +0000

    Create Centaine.cs

---
+++ ChiffreEnLettre/Centaine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChiffreEnLettre
+{
+    public class Centaine:Dizaine
+    {
+        public static string CentaineInLetter(long i)
+        {
+            string cent, rslt;
+
+            //modulo
+            long k = i % 100;
+
+            //long a = i - k;
+            //long cnt = a / 100;
+
+            long cnt = (i - k) / 100;
+
+            cent = UniteCentaineInLetter(cnt);
+
+            if (k.IsUnite())
+            {
+                //Unite
+                rslt = cent + " cent " + UniteDizaineCentaineInLetter(k);
+            }
+            else
+            {
+                //Dizaine
+                rslt = cent + " cent " + (DizainesInLetter(k)).ToLower();
+            }
+
+            return rslt.FirstToUpperCase().Trim();
+        }
+    }
+}
