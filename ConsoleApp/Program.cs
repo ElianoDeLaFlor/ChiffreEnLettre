056a848442f0fd846ec15b4efe25bdcb0d10e7b2
ElianoDeLaFlor <48012583+ElianoDeLaFlor@users.noreply.github.com>
2020-01-31 15:59:31 +0000

    Create Program.cs

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using ChiffreEnLettre;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nous allons tester notre code avant de poursuivre
+            string k = "";
+            do
+            {
+                Console.WriteLine("Votre nombre");
+                string nbr = Console.ReadLine();
+                EnLettre.Chiffre = nbr;
+                Console.WriteLine(EnLettre.Resultat);
+                Console.WriteLine("Appuyer sur O pour rééssayer");
+                k = Console.ReadLine();
+            } while (k == "O");
+        }
+    }
+}
