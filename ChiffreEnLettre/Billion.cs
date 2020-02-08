14d0bb3390adc397aecd275e6e5a1e632a999a4a
ElianoDeLaFlor <48012583+ElianoDeLaFlor@users.noreply.github.com>
2020-02-08 15:35:43 +0000

    Create Billion.cs

---
+++ ChiffreEnLettre/Billion.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChiffreEnLettre
+{
+    public class Billion:Millionieme
+    {
+        public static string BillionInLetter(long l)
+        {
+            string billion, rslt;
+            
+            //modulo
+            long k = l % 1000000000;
+
+            long billionn = (l - k) / 1000000000;
+
+            if (billionn.IsUnite())
+            {
+                //Unite
+                billion = InLetter(billionn);
+            }
+            else if (billionn.IsDizaine())
+            {
+                //Dizaine
+                billion = $"{DizainesInLetter(billionn)}";
+            }
+            else if (billionn.IsCentaine())
+            {
+                //Centaine
+                billion = $"{CentaineInLetter(billionn)}";
+            }
+            else if (billionn.IsMillieme())
+            {
+                //millième
+                billion = $"{MilleInLetter(billionn)}";
+            }
+            else
+            {
+                //Million
+                billion = MillionInLetter(billionn);
+            }
+
+            if (k.IsUnite())
+            {
+                //Unite
+                rslt = $"{billion} Milliard {UniteDizaineCentaineInLetter(k)}";
+            }
+            else if (k.IsDizaine())
+            {
+                //Dizaine
+                rslt = $"{billion} Milliard {DizainesInLetter(k)}";
+            }
+            else if (k.IsCentaine())
+            {
+                //Centaine
+                rslt = $"{billion} Milliard {CentaineInLetter(k)}";
+            }
+            else if(k.IsMillieme())
+            {
+                //Mille
+                rslt = $"{billion} Milliard {MilleInLetter(k)}";
+            }
+            else //if (k.IsMillionnieme())
+            {
+                //Million
+                Millionieme.IsBillion = true;
+                rslt = $"{billion} Milliard {MillionInLetter(k)}";
+            }
+            return rslt.Trim().FirstToUpperCase();
+        }
+    }
+}
